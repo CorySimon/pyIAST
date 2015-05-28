b398e8e5189f60a3bd1dc4af9a735e273ff8c0cb
CorySimon <corysimon@berkeley.edu>
2015-05-28 16:24:51 -0700

    added test

--- src/setup.py
+++ src/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='IAST',
     version='0.0',
     description='Ideal Adsorbed Solution Theory',
-    url='https://github.com/CorySimon/IAST.jl',
+    url='https://github.com/CorySimon/IAST',
     install_requires=['numpy', 'scipy', 'pandas', 'matplotlib'],
     author='Cory M. Simon',
     author_email='CoryMSimon@gmail.com')
