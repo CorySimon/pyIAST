19d5ba253f6c767b8ce64ff7ce222ed6134bf247
Cory Simon <corymsimon@barracuda.lbl.gov>
2015-05-28 13:35:38 -0700

    first commit

---
+++ src/IAST.py
@@ -0,0 +1,94 @@
+__author__ = 'Cory M. Simon'
+
+from isotherms import *
+import scipy.optimize
+
+
+def IAST(p, isotherms, verboseflag=False):
+    """
+    Perform IAST for a mixture, given pure-component isotherms 
+    
+    :param p: Array partial pressures of components
+    :param isotherms list of pure-component adsorption isotherms
+    :param verboseflag: bool print stuff
+    :return: q: array of predicted uptakes of each component
+    """
+    n_components = len(isotherms)  # number of components in the mixture
+    if n_components == 1:
+        raise Exception("Pass list of pure component isotherms...")
+
+    if (np.size(p) != n_components):
+        print """Example use:\n
+              IAST([0.5,0.5], [xe_isotherm, kr_isotherm], verboseflag=true)"""
+        raise Exception("Length of partial pressures != length of array of isotherms...")
+
+    if verboseflag:
+        print "%d components." % n_components
+        for i in range(n_components):
+            print "\tPartial pressure component %d = %f" % (i, p[i])
+
+    # assert that the spreading pressures of each component are equal
+    def spreading_pressure_differences(x):
+        """
+        Assert that spreading pressures of each component are equal.
+
+        :param x: array mole fractions in the adsorbed phase
+        np.size(x) = n_components - 1 because \sum x_i = 1 asserted here automatically.
+        :returns: spreading_pressure_diff: array spreading pressure difference between component i and i+1
+        """
+        spreading_pressure_diff = np.zeros((n_components - 1,))
+        for i in range(n_components - 1):
+            if i == n_components - 2:
+                xn = 1.0 - np.sum(x)  # automatically assert \sum x_i = 1
+                spreading_pressure_diff[i] = isotherms[i].spreading_pressure(p[i] / x[i]) - isotherms[
+                    i + 1].spreading_pressure(p[i + 1] / xn)
+            else:
+                spreading_pressure_diff[i] = isotherms[i].spreading_pressure(p[i] / x[i]) - isotherms[
+                    i + 1].spreading_pressure(p[i + 1] / x[i + 1])
+        return spreading_pressure_diff
+    
+    # solve for mole fractions in adsorbed phase by equating spreading pressures
+    guess = p / np.sum(p)
+    n_tries = 0  # try with many different guesses until result found
+    while n_tries < 10:
+     #     res = scipy.optimize.root(spreading_pressure_differences, guess[:-1])
+        x = scipy.optimize.fsolve(spreading_pressure_differences, guess[:-1])
+
+        # mole fraction in adsorbed phase
+        x = np.concatenate((x, np.array([1 - np.sum(x)])))
+        n_tries += 1
+        # check if feasible soln is found
+        if ((np.sum(x >= 0.0) == n_components) & (np.sum(x <= 1.0) == n_components)):
+            break
+        guess = np.random.uniform(size=n_components)
+        guess = guess / np.sum(guess)
+
+ #     x = np.concatenate((res.x, np.array([1 - np.sum(res.x)])))
+    if ((np.sum(x < 0.0) != 0) | (np.sum(x > 1.0) != 0)):
+        print "Tried %d times" % n_tries
+        for i in range(n_components):
+            print "\tx[%d] = %f\n" % (i, x[i])
+            print "\tGuess: ", guess
+        raise Exception("x not in [0,1], solution infeasible...")
+
+    p0 = p / x
+
+    # solve for the total gas adsorbed
+    denom = 0.0
+    for i in range(n_components):
+        denom += x[i] / isotherms[i].loading(p0[i])
+    q_total = 1.0 / denom
+
+    # get loading of each component by multiplying by mole fractions
+    q = x * q_total
+    if verboseflag:
+        # print off loadings according to IAST and corresponding pure component loadings
+        for i in range(n_components):
+            print "Component ", i
+            print "\tp = ", p[i]
+            print "\tLoading: ", q[i]
+            print "\tx = ", x[i]
+            print "\tSpreading pressure = ", isotherms[i].spreading_pressure(p0[i])
+            print "\tPure component loading at same partial pressure = ", isotherms[i].loading(p[i])
+
+    return q  # loadings [component 1, component 2, ...]. same units as in data
---
+++ src/isotherms.py
@@ -0,0 +1,260 @@
+###
+#   Isotherm models
+#   Author: CoryMSimon@gmail.com
+###
+__author__ = 'Cory M. Simon'
+
+import scipy.optimize
+from scipy.interpolate import interp1d
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
+class LangmuirIsotherm:
+    """
+    Langmuir isotherm object
+    """
+
+    def __init__(self, df, loading_key=None, pressure_key=None):
+        """
+        Instantiation
+
+        :param df: DataFrame adsorption isotherm data
+        :param loading_key: String key for loading column in df
+        :param pressure_key: String key for pressure column in df
+
+        :returns: self
+        :rtype: LangmuirIsotherm
+        """
+        # store isotherm data in self
+        self.df = df
+        if loading_key==None or pressure_key == None:
+            raise Exception("Provide names of pressure and loading cols in dataframe")
+        self.loading_key = loading_key
+        self.pressure_key = pressure_key
+
+        def RSS(params):
+            """
+            Residual Sum of Squares between Langmuir model and data in df
+            :param params: Array params = [K, M]
+            M: float saturation loading (units: loading)
+            K: float Langmuir constant (units: 1/pressure)
+            """
+            return np.sum((df[loading_key].values -
+                           params[1] * params[0] * df[pressure_key].values /
+                           (1.0 + params[0] * df[pressure_key].values)) ** 2)
+
+        # for guess as starting point in minimizing RSS
+        M_guess = np.max(df[pressure_key].values)  # guess saturation loading to be highest loading
+        # guess K using M_guess and lowest pressure point
+        idx_min = np.argmin(df[loading_key].values)
+        K_guess = df[loading_key].iloc[idx_min] / df[pressure_key].iloc[idx_min] / (
+            M_guess - df[pressure_key].iloc[idx_min])
+
+        # minimize RSS
+        opt_res = scipy.optimize.minimize(RSS, [K_guess, M_guess], method='Nelder-Mead')
+        if opt_res.success == False:
+            print(opt_res.message)
+            print "M_guess = ", M_guess
+            print "K_guess = ", K_guess
+            raise Exception("Minimization of RSS for Langmuir isotherm fitting failed... Try a different guess.")
+
+        # assign params
+        self.K = opt_res.x[0]
+        self.M = opt_res.x[1]
+
+    def loading(self, P):
+        """
+        Given stored Langmuir parameters, compute loading at pressure P
+
+        :param P: float pressure (in corresponding units as df in instantiation)
+        :return: float loading at pressure P (in corresponding units as df in instantiation)
+        """
+        return self.M * (self.K * P / (1.0 + self.K * P))
+
+    def spreading_pressure(self, P):
+        """
+        Calculate spreading pressure at a bulk gas pressure P
+
+        :param P: float pressure (in corresponding units as df in instantiation)
+        :return: float spreading pressure
+        """
+        return self.M * np.log(1.0 + self.K * P)
+
+
+
+class QuadraticIsotherm:
+    """
+    Quadratic isotherm object
+    eqn (8) of Tarafder
+    """
+
+    def __init__(self, df, loading_key=None, pressure_key=None):
+        """
+        Instantiation
+
+        :param df: DataFrame adsorption isotherm data
+        :param loading_key: String key for loading column in df
+        :param pressure_key: String key for pressure column in df
+
+        :return: self
+        :rtype: QuadraticIsotherm 
+        """
+        # store isotherm data in self
+        self.df = df
+        if loading_key==None or pressure_key == None:
+            raise Exception("Provide names of pressure and loading cols in dataframe")
+        self.loading_key = loading_key
+        self.pressure_key = pressure_key
+
+        def RSS(params):
+            """
+            Residual Sum of Squares between Quadratic isotherm model and data in df
+            :param params: Array params = [Ka, Kb, M]
+            M: float saturation loading (units: loading)
+            Ka, Kb: float Quadratic isotherm constants (units: 1/pressure, 1/pressure^2)
+            """
+            return np.sum((df[loading_key].values -
+                           params[2] * (params[0] + 2.0 * params[1] * df[pressure_key].values) * df[
+                               pressure_key].values /
+                           (1.0 + params[0] * df[pressure_key].values + params[1] * df[pressure_key].values ** 2)) ** 2)
+
+        # for guess as starting point in minimizing RSS
+        M_guess = np.max(df[pressure_key].values) / 2.0  # guess saturation loading to be highest loading
+        # guess K using M_guess and lowest pressure point
+        idx_min = np.argmin(df[loading_key].values)
+        Ka_guess = df[loading_key].iloc[idx_min] / df[pressure_key].iloc[idx_min] / (
+            M_guess - df[pressure_key].iloc[idx_min])
+        Kb_guess = 0.000000001  # guess as Langmuir adsorption isotherm at first
+
+        # minimize RSS
+        opt_res = scipy.optimize.minimize(RSS, [Ka_guess, Kb_guess, M_guess], method='Nelder-Mead')
+        if opt_res.success == False:
+            print(opt_res.message)
+            print "M_guess = ", M_guess
+            print "Ka_guess = ", Ka_guess
+            print "Kb_guess = ", Kb_guess
+            raise Exception("Minimization of RSS for Quadratic isotherm fitting failed... Try a different guess.")
+
+        # assign params
+        self.Ka = opt_res.x[0]
+        self.Kb = opt_res.x[1]
+        self.M = opt_res.x[2]
+
+    def loading(self, P):
+        """
+        Given stored Langmuir parameters, compute loading at pressure P
+
+        :param P: float pressure (in corresponding units as df in instantiation)
+        :return: float loading at pressure P (in corresponding units as df in instantiation)
+        """
+        return self.M * (self.Ka + 2.0 * self.Kb * P) * P / (1.0 + self.Ka * P + self.Kb * P ** 2)
+
+    def spreading_pressure(self, P):
+        """
+        Calculate spreading pressure at a bulk gas pressure P
+
+        :param P: float pressure (in corresponding units as df in instantiation)
+        :return: float spreading pressure
+        """
+        return self.M * np.log(1.0 + self.Ka * P + self.Kb * P ** 2)
+
+
+class InterpolatorIsotherm:
+    """
+    Interpolator isotherm object
+    """
+
+    def __init__(self, df, loading_key=None, pressure_key=None, fill_value=None):
+        """
+        Instantiation
+
+        :param df: DataFrame adsorption isotherm data
+        :param loading_key: String key for loading column in df
+        :param pressure_key: String key for pressure column in df
+        :param fill_value: Float value to assume when interp1d goes outside bounds
+
+        :return: self
+        :rtype: InterpolatorIsotherm 
+        """
+        # if pressure = 0 not in data frame, add it for interpolation between 0 and first point.
+        if 0.0 not in df[pressure_key].values:
+            df = pd.concat([pd.DataFrame({pressure_key:0.0, loading_key:0.0}, index=[0]), df])
+
+        # store isotherm data in self
+        self.df = df.sort([pressure_key], ascending=True)
+        if loading_key==None or pressure_key == None:
+            raise Exception("Provide names of pressure and loading cols in dataframe")
+        self.loading_key = loading_key
+        self.pressure_key = pressure_key
+        
+        if fill_value == None:
+            self.interp1d = interp1d(df[pressure_key], df[loading_key])
+        else:
+            self.interp1d = interp1d(df[pressure_key], df[loading_key], fill_value=fill_value, bounds_error=False)
+
+    def loading(self, P):
+        """
+        Given stored Langmuir parameters, compute loading at pressure P
+
+        :param P: float pressure (in corresponding units as df in instantiation)
+        :return: float loading at pressure P (in corresponding units as df in instantiation)
+        """
+        return self.interp1d(P)
+
+    def spreading_pressure(self, P):
+        """
+        Calculate spreading pressure at a bulk gas pressure P
+        Use trapezoid rule on isotherm
+        \int_0^p q(\hat{p})/ \hat{p} d\hat{p} 
+        eqn 4 of Tarafder
+
+        :param P: float pressure (in corresponding units as df in instantiation)
+        :return: float spreading pressure
+        """
+        # get all data points less than this P. Do not use P = 0.0, this will give nan.
+        idx = (self.df[self.pressure_key].values < P) & (self.df[self.pressure_key].values != 0.0)
+        if np.sum(idx) == 0:
+            # if this pressure is between 0 and first pressure point
+            l_array = float(self.loading(P))
+            return l_array / 2.0 # area of triangle starting at P = 0
+        else:
+            # (1/2 * p1 * [height=loading/p1]
+            # area of first triangle in integral, from 0 to first pressure point
+            A_first_triangle = self.df[self.loading_key].iloc[idx][0] / 2.0
+            p_array = np.concatenate((self.df[self.pressure_key].iloc[idx], np.array([P])))  # array of pressures
+            l_array = np.concatenate(
+                (self.df[self.loading_key].iloc[idx], np.array([self.loading(P)])))  # array of loadings
+            return np.trapz(l_array / p_array, x=p_array) + A_first_triangle
+
+
+def plot_isotherm(isotherm, withfit=True, xlogscale=False, ylogscale=False):
+    """
+    Plot isotherm data and fit
+    
+    :param isotherm: LangmuirIsotherm,QuadraticIsotherm,InterpolatorIsotherm the adsorption isotherm object
+    :param withfit: Bool plot fit as well
+    :param xlogscale: Bool log-scale on x-axis
+    :param ylogscale: Bool log-scale on y-axis
+    """
+
+    fig = plt.figure()
+    if withfit:
+        # array of pressures to plot model
+        if xlogscale:
+            P = np.logspace(np.log(isotherm.df[isotherm.pressure_key].min()), np.log(isotherm.df[isotherm.pressure_key].max()), 200)
+            plt.plot(P, isotherm.loading(P))
+        else:
+            P = np.linspace(isotherm.df[isotherm.pressure_key].min(), isotherm.df[isotherm.pressure_key].max(), 200)
+            plt.plot(P, isotherm.loading(P))
+    plt.scatter(isotherm.df[isotherm.pressure_key], isotherm.df[isotherm.loading_key])
+    if xlogscale:
+        plt.xscale("log")
+    if ylogscale:
+        plt.yscale("log")
+    plt.xlim(xmin=0.0)
+    plt.ylim(ymin=0.0)
+    plt.xlabel('Pressure')
+    plt.ylabel('Loading')
+    plt.show()
