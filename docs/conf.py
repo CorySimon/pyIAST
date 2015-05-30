30ed02a9ecddd43517b98c74b5becedb0c4004b1
Cory <cory@ubuntu.ubuntu-domain>
2015-05-30 11:36:41 -0700

    conf.py added to mock isotherms and iast...

--- docs/conf.py
+++ docs/conf.py
@@ -21,7 +21,7 @@ class Mock(MagicMock):
     def __getattr__(cls, name):
         return Mock()
 
-MOCK_MODULES = ['pandas', 'numpy', 'pandas', 'scipy', 'scipy.interpolate', 'scipy.optimize', 'matplotlib', 'matplotlib.pyplot']
+MOCK_MODULES = ['pandas', 'numpy', 'pandas', 'scipy', 'scipy.interpolate', 'scipy.optimize', 'matplotlib', 'matplotlib.pyplot', 'isotherms', 'IAST']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 # If extensions (or modules to document with autodoc) are in another directory,
