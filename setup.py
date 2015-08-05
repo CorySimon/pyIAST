0c13663b390bbfb7ec8d7a1bb590a02a5e65b2be
Cory Simon <corysimon@berkeley.edu>
2015-08-05 14:18:42 -0700

    Delete setup.py

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup
-
-setup(name='IAST',
-    version='0.0',
-    description='Ideal Adsorbed Solution Theory',
-    url='https://github.com/CorySimon/IAST',
-    install_requires=['numpy', 'scipy', 'pandas', 'matplotlib'],
-    author='Cory M. Simon',
-    author_email='CoryMSimon@gmail.com')
