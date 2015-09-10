af0e3e83411f7132840c98f167a59295aab9bd04
CorySimon <corysimon@berkeley.edu>
2015-09-10 11:25:42 -0700

    for rtd

--- docs/conf.py
+++ docs/conf.py
@@ -12,9 +12,9 @@
 # serve to show the default.
 
 import sys, os
- # from mock import Mock as MagicMock  # for local make html
+from mock import Mock as MagicMock  # for local make html
  # import sphinx_rtd_theme # for local
-from unittest.mock import MagicMock
+ # from unittest.mock import MagicMock
 
 class Mock(MagicMock):
     @classmethod
